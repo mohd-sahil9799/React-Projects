ed1b50a9f201a38fe482fddc96cae7b8ad7d084b
MOHD SAHIL <[200~mohd.sahil@aarvytechnologies.co.in>
2023-12-28 14:50:25 +0530

    this is a demo folder

--- project/src/component/Api.jsx
+++ project/src/component/Api.jsx
@@ -1,40 +1,44 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function Api() {
-  const [input, setInput] = useState("");
-  const handleInputChange = (e) => {
-    setInput(e.target.value);
-  };
 
-  const sendPostRequest = () => {
-  const item = {
-    userId: 1,
-    title:{input},
-    body: "API Body",
-  }
+function App() {
+  // Your trending searches data (replace this with your actual trending searches)
+  const trendingSearches = ['Events', 'Bhagavad Gita', 'Music', 'Podcast'];
 
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      body: JSON.stringify(item),
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-    })
-    .then((response) => response.json())
-    .then((json) => {
-      console.log(json)
-    });
+  const openSecondModal = () => {
+    // Code to open second modal (e.g., using Bootstrap's modal API or React Bootstrap methods)
   };
 
   return (
     <div>
-      <form>
-        <input type="text" value={input} onChange={handleInputChange} />
-        <button type="button" onClick={sendPostRequest}>Send POST Request</button>
-      
-      </form>
+      {/* Search Modal */}
+      <Modal id="exampleModal" show={false}>
+        {/* Content of your Search Modal */}
+        {/* ... */}
+        {/* Example of Trending Searches */}
+        <div className="Trending">
+          <h5>Trending Searches</h5>
+          {trendingSearches.map((search, index) => (
+            <div key={index} className="border d-flex justify-content-between px-3 py-2">
+              <p>{search}</p>
+              <i className="fas fa-arrow-left fs-4" style={{ transform: 'rotate(45deg)' }} onClick={openSecondModal}></i>
+            </div>
+          ))}
+        </div>
+        {/* ... */}
+      </Modal>
+
+      {/* Second Modal */}
+      <Modal id="exampleModal1" show={false}>
+        {/* Content of your Second Modal */}
+        {/* ... */}
+        <div className="modal-body">
+          <p>Content of Second Modal</p>
+        </div>
+        {/* ... */}
+      </Modal>
     </div>
   );
 }
 
-export default Api;
+export default App;
